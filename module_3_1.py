e5aad8cbd6a5272473011d250ba1ee2ac505c96c
deemon1988 <dmn72835@gmail.com>
2024-10-18 12:08:11 +0300

    Задача "Счётчик вызовов" - Complete

--- module_3_1.py
+++ module_3_1.py
@@ -4,25 +4,22 @@ def count_calls():
     global calls
     calls += 1
 
-def string_info(string:str)-> tuple:
+def string_info(*,string:str)-> tuple:
     count_calls()
     return len(string), string.upper(), string.lower()
 
-def is_contains(string, list_to_search)-> bool:
+def is_contains(*,string:str, list_to_search:list)-> bool:
     count_calls()
     for i in list_to_search:
-        print(i.upper())
         if string.upper() == i.upper():
             return True
-        # else:
-        #     continue
     return False
 
 
-string_info("Hello World")
-list_ = ["world", "heLlo","WOrld", "Urban", "python"]
-print(is_contains("python", list_))
-
+string_info(string="Hello World")
+print(string_info(string="Hello World"))
+list_ = ["world", "heLlo","WOrld", "Urban", "pYtHon"]
+print(is_contains(string="Python", list_to_search=list_))
 
 print(calls)
 
