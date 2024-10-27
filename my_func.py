a7d9176c1a883c6692d62eed649ab3ed2d592cb3
deemon1988 <dmn72835@gmail.com>
2024-10-28 02:33:13 +0300

    Function for parsing in txt file

--- my_func.py
+++ my_func.py
@@ -2,24 +2,23 @@ import string
 
 str1 = 'Avery Breakwell abreakwell2@weather.com 850-192-9967'
 f= open('example.txt','r')
-#print(*f)
-#tuple_ =[].append(*f.readlines())
-#print(tuple_)
-#print(*f.readlines())
 
+def get_user_data(*info: str) -> list:
+    r_list = []
+    for i in info:
+        data = i.replace("\t", ' ')
+        data = data.replace("\n", '')
+        data_list = data.split(' ')
+        u_email = mail(data_list)
+        u_phone = phone(data_list)
+        data_list.remove(u_email)
+        data_list.remove(u_phone)
+        u_name = ' '.join(data_list)
+        u_phone = '('+u_phone[:3]+')' +u_phone[3:]
+        u_data = f"'{u_name}', '{u_phone}', '{u_email}'"
 
-def get_user_data(info: str) -> str:
-    data = info.replace("\t", ' ')
-    data_list = data.split(' ')
-    u_email = mail(data_list)
-    u_phone = phone(data_list)
-    data_list.remove(u_email)
-    data_list.remove(u_phone)
-    u_name = ' '.join(data_list)
-    u_phone = '('+u_phone[:3]+')' +u_phone[3:]
-    #u_data = ', '.join([u_name,u_email,u_phone])
-    u_data = f"'{u_name}', '{u_phone}', '{u_email}'"
-    return f"({u_data})"
+        r_list.append(f"({u_data})")
+    return r_list
 
 
 
@@ -28,7 +27,7 @@ def mail(info: list) -> str:
     for str_ in info:
         if '@' in str_:
             email = str_
-        elif str_.isdigit() and 1 < len(str_) < 15 and '-' not in str_:
+        elif str_.isdigit() and 0 < len(str_) < 15 and '-' not in str_:
             info.remove(str_)
     return email
 
@@ -41,17 +40,20 @@ def phone(info: list):
                 is_phone = i
     return is_phone
 
-def get_tuple(*values):
-    list_ = []
-    str_ = values[0]
-    for i in values:
-        list_.append(i)
-    print(values)
-    print(str_)
-get_tuple(*f)
+print("User data:",get_user_data(*f,))
+f.close()
+
+f= open('example.txt','r')
+list_for_write = get_user_data(*f,)
+f.close()
+
+with open('output.txt','w') as f_out:
+    for line in list_for_write:
+        f_out.write(f"{line},\n")
+
+f_out.close()
 
 
-print("User data:",get_user_data(str1))
 
 
 
