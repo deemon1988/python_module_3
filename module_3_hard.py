f337c472f3addaf64f16b8252f630ed586dc535c
deemon1988 <dmn72835@gmail.com>
2024-10-29 19:45:43 +0300

    better Count value for the list elements with Recursive - 2 Working fucntions

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,5 +1,3 @@
-from bisect import insort
-from reprlib import recursive_repr
 
 data_structure = [
     [1, 2, 3],
@@ -10,9 +8,12 @@ data_structure = [
 ]
 
 
+for i in data_structure:
+    if isinstance(i, list):
+       *list_, = i
+
+
 tuple_ = ((), [{(2, 'Urban', ('Urban2', 35))}])
-
-
 
 
 def unpack_recursive(struc):
@@ -26,7 +27,6 @@ def unpack_recursive(struc):
     if  len(one_element) > 0 and isinstance(one_element, list|tuple|str|int):
         summ = 0
         for i in one_element:
-            # while i <= len(one_element):
             if isinstance(i, int):
                 summ += i
             elif isinstance(i, str):
@@ -35,8 +35,6 @@ def unpack_recursive(struc):
                 return summ + unpack_recursive(i)
         else:
             return summ + unpack_recursive(struc[1:])
-            # return unpack_recursive(i)
-
     else:
         return sum(struc[:1])
 
@@ -46,28 +44,24 @@ print(many_tuple)
 
 def calculate_structure_sum(data_structure):
     if len(data_structure)>0:
+        summ = 0
+        k = 0
+        w = 0
         for i in (data_structure):
             if isinstance(i, str):
                 str_=len(i)
                 return str_ + calculate_structure_sum(data_structure[1:])
             if isinstance(i, tuple):
-               # first = i[:1]
-                summ = 0
                 for j in i:
                     if isinstance(j, int):
                         summ += j
                     elif isinstance(j, dict):
-                        k = 0
-                        w = 0
                         for d, v in j.items():
                             k += len(d)
                             w += v
                         summ += k + w
                 return summ + calculate_structure_sum(data_structure[1:])
             elif isinstance(i, dict):
-                k=0
-                w=0
-                summ= 0
                 for d,v in i.items():
                     k += len(d)
                     w += v
